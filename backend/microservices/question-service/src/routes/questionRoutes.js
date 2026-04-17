da9d6613da9088a9d7070cc3cf4cdb982ca7653b
Ritwik <devilrqop@gmail.com>
2026-04-17 08:27:46 +0530

    Wiped repository

--- backend/microservices/question-service/src/routes/questionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { fetchQuestions } = require("../controllers/questionController");
-
-const router = express.Router();
-
-router.get("/fetch", fetchQuestions);
-
-module.exports = router;
--- frontend/src/lib/user-actions.ts
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { useUser } from '@clerk/nextjs';
-
-export function useUserData() {
-  const { user, isLoaded } = useUser();
-  
-  return { user, isLoaded };
-}
